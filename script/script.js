cd9f21f0cf37d3c5ad1eb5cf09511e351276e3f5
Oleksii <lesha.avdeev@gmail.com>
2023-02-11 15:41:44 +0200

    Add calculation functionality

--- script/script.js
+++ script/script.js
@@ -4,12 +4,14 @@ const calcBtn = document.querySelectorAll(".calc-btn");
 const output = document.querySelector("#output");
 const acBtn = document.querySelector("#ac-btn");
 const opBtn = document.querySelectorAll(".op-btn");
+const solBtn = document.querySelector("#sol-btn");
 
 let calcList = [];
 let idx = 0;
 
-acBtn.addEventListener('click', clear);
+solBtn.addEventListener('click', solve);
 
+acBtn.addEventListener('click', clear);
 
 calcBtn.forEach(element => {
     element.addEventListener('click', addNum);    
@@ -35,7 +37,7 @@ function divide(x, y) {
     return x / y;
 }
 
-function operate(operator, x, y) {
+function operate(x, operator, y) {
     let operationResult;
 
     if (operator === "+") {
@@ -61,8 +63,26 @@ function addNum() {
 }
 
 function addOp() {
-    if (displayVal.charAt(displayVal.length - 1).match(/[0-9]/) !== null) {
+    let currLength = displayVal.length;
+    if (displayVal.charAt(currLength - 1).match(/[0-9]/) !== null) {
+        calcList.push(displayVal.slice(idx));
+        calcList.push(this.textContent);
+        console.log(calcList)
+        idx = currLength + 3;
         displayVal += ` ${this.textContent} `;
         output.textContent = displayVal;
+    }
+}
+
+function solve() {
+    if (displayVal.charAt(idx).match(/[0-9]/) !== null) {
+        calcList.push(displayVal.slice(idx));
+        let opNum = Math.floor(calcList.length / 2);
+        for(let i = 0; i < opNum; i++) {
+            calcList.unshift(operate(parseFloat(calcList.shift()), calcList.shift(), parseFloat(calcList.shift())));
+        }
+        displayVal = `${calcList[0]}`;
+        idx = 0;
+        output.textContent = calcList.shift();
     }
 }
