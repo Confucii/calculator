b239121611b718d6a150422a950d4139fc6ba385
Oleksii <lesha.avdeev@gmail.com>
2023-02-11 14:19:26 +0200

    Add display functionality for numbers and operators

--- script/script.js
+++ script/script.js
@@ -1,3 +1,24 @@
+let displayVal = "";
+
+const calcBtn = document.querySelectorAll(".calc-btn");
+const output = document.querySelector("#output");
+const acBtn = document.querySelector("#ac-btn");
+const opBtn = document.querySelectorAll(".op-btn");
+
+let calcList = [];
+let idx = 0;
+
+acBtn.addEventListener('click', clear);
+
+
+calcBtn.forEach(element => {
+    element.addEventListener('click', addNum);    
+});
+
+opBtn.forEach(element => {
+    element.addEventListener('click', addOp);    
+});
+
 function add(x, y) {
     return x + y;
 }
@@ -27,4 +48,21 @@ function operate(operator, x, y) {
         operationResult = divide(x, y);
     }
     return operationResult;
+}
+
+function clear() {
+    displayVal = "";
+    output.textContent = "";
+}
+
+function addNum() {
+    displayVal += this.textContent;
+    output.textContent = displayVal;
+}
+
+function addOp() {
+    if (displayVal.charAt(displayVal.length - 1).match(/[0-9]/) !== null) {
+        displayVal += ` ${this.textContent} `;
+        output.textContent = displayVal;
+    }
 }
